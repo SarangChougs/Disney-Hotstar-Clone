2d20886898d657b65662abc2e298c5fa458551e2
sarangchouguley <sarangchouguley284@gmail.com>
2023-06-10 02:07:50 +0530

    recomment component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,62 @@
 import styled from "styled-components"
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommends";
+import Trending from "./Trending";
 import Viewers from "./Viewers";
+import disneyMoviesData from '../disneyPlusMoviesData.json';
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const shouldUseEffect = useRef(true);
+  const moviesData = useRef(new Array());
+
+  const [movies, setMovies] = useState([]); 
+  let recommends = [];
+  let newDisney = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    if(shouldUseEffect.current){
+      shouldUseEffect.current = false;
+      //create array of object from moviesdata json file
+      Object.entries(disneyMoviesData.movies).forEach((movie) => {
+        const [key,value] = movie;
+        moviesData.current.push({id: key, ...value});
+      });
+      console.log(moviesData)
+      
+      // add movies in each movie type array according to movie type
+      moviesData.current.map(movie => {
+        switch(movie.type) {
+          case "recommend": 
+            recommends.push({id: movie.id, ...movie});
+            break;
+          case "new":
+            newDisney.push({id: movie.id, ...movie});
+            break;
+          case "original":
+            originals.push({id: movie.id, ...movie});
+            break;
+          case "trending":
+            trending.push({id: movie.id, ...movie});
+            break;
+        }
+      });
+    }
+    setMovies([...movies, recommends, newDisney, originals, trending]);
+  }, []);
+
   return (
     <Container>
         <ImgSlider/>
         <Viewers/>
+        <Recommends movies={movies[0]}/>
+        <NewDisney movies={movies[1]}/>
+        <Originals movies={movies[2]}/>
+        <Trending movies={movies[3]}/>
     </Container>
   )
 }
